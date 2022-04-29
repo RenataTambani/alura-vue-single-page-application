7306abae5979105b6ee46535c246206a4dc672e6
RenataTambani <renatatambani@gmail.com>
2022-04-29 17:32:16 -0300

    component-painel

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
+
+
 <template>
-  <div id="app">
-   <h1>{{ titulo }}</h1>
-   <ul>
-     <li v-for="foto in fotos" :key="foto.id">
-       <img :src="foto.url" :alt="foto.titulo">
-     </li>
-  </ul>
+  <div id="app" class="corpo">
+   <h1 class="centralizado">{{ titulo }}</h1>
+    <ul class="lista-fotos">
+      <li v-for="foto in fotos" :key="foto.id" class="lista-fotos-item"> 
+        <meu-painel :titulo="foto.titulo">
+          <img :src="foto.url" :alt="foto.titulo" class="imagem-responsiva">
+        </meu-painel>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import Painel from './components/shared/painel/painel.vue'
+
 export default {
- 
+
+  components: {
+    "meu-painel": Painel
+  },
   data() { 
     return {
     titulo: 'Alurapic',  
@@ -20,16 +29,33 @@ export default {
      
     }
   },
-    created() {
-      let promise = this.$http.get('http://localhost:3000/v1/fotos')
-        .then(r => r.json())
-        .then(fotos1 => this.fotos = fotos1)
-     
-  
+  created() {
+    let promise = this.$http.get('http://localhost:3000/v1/fotos')
+      .then(r => r.json())
+      .then(fotos1 => this.fotos = fotos1)
   },
-
 }
 </script>
 
+
 <style>
+
+  .centralizado {
+    text-align: center;
+  }
+
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
 </style>
