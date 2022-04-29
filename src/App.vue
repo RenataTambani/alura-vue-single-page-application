cb54002d702747a12edcf7e7f3100499f683cd78
RenataTambani <renatatambani@gmail.com>
2022-04-29 16:39:42 -0300

    commit-inicial

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+   <h1>{{ titulo }}</h1>
+   <ul>
+     <li v-for="foto in fotos" :key="foto.id">
+       <img :src="foto.url" :alt="foto.titulo">
+     </li>
+  </ul>
+  </div>
+</template>
+
+<script>
+export default {
+ 
+  data() { 
+    return {
+    titulo: 'Alurapic',  
+     fotos: [ ]
+    
+     
+    }
+  },
+    created() {
+      let promise = this.$http.get('http://localhost:3000/v1/fotos')
+        .then(r => r.json())
+        .then(fotos1 => this.fotos = fotos1)
+     
+  
+  },
+
+}
+</script>
+
+<style>
+</style>
